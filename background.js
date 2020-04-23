034d001c1a8d586467b5a3aa155db9bc567fd8e9
Steven Pham <stevenpham408@yahoo.com>
2020-04-22 20:48:52 -0700

    (1) Added event listener to use variables from popup.js
    (2) Will now obtain the time unit that the user selected
    
    (3) Other changes such as removing code from the provided Google example 
    that I built this code from.

--- background.js
+++ background.js
@@ -2,20 +2,37 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+
 'use strict';
+
 var numToStop = 0;
+var timeUnit;
+var toggle;
+
 function delayedDelete(tab){
-	  setTimeout(function(){
+	console.log("delayedDelete");
+	console.log(timeUnit);
+	setTimeout(function(){
 		chrome.tabs.remove(tab.id)
-    console.log("Time: ", new Date().toString());
-	}, 45000);
+		console.log("Time: ", new Date().toString());
+	}, 5000 );
 }
 
 var numToDelete = 0;
+
 chrome.runtime.onInstalled.addListener(function() {
-  chrome.storage.sync.set({color: '#3aa757'}, function() {
-    console.log("The color is green.");
-  });
+	chrome.storage.sync.set({color: '#3aa757'}, function() {
+		console.log("The extension is running.");
+	});
+
+	// chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+	// 	chrome.declarativeContent.onPageChanged.addRules([{
+	// 		conditions: [new chrome.declarativeContent.PageStateMatcher({
+	// 			pageUrl: {hostEquals: 'developer.chrome.com'},
+	// 		})],
+	// 		actions: [new chrome.declarativeContent.ShowPageAction()]
+	// 	}]);
+	// });
 });
 
 var openTabs = {};
@@ -26,7 +43,10 @@ chrome.tabs.onCreated.addListener(function(tab) {
   numToDelete = numToDelete + 1;
   console.log(openTabs[tab.id].toString());
   console.log("Length: ", numToDelete);
-  delayedDelete(tab);
+	while(toggle == true)
+	{
+		delayedDelete(tab);
+	}
 });
 
 chrome.tabs.onRemoved.addListener(function(tabID, removeInfo) {
@@ -38,3 +58,29 @@ chrome.tabs.onRemoved.addListener(function(tabID, removeInfo) {
     console.log("Else: ")
   }
 });
+
+
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    console.log(sender.tab ?
+                "from a content script:" + sender.tab.url :
+                "from the extension");
+    if (request.message == "Sending selected time and toggle option")
+      sendResponse({received: "Received selected time option"});
+			timeUnit = request.varTime;
+			toggle = request.varToggle;
+			console.log("Sex", timeUnit, toggle);
+			return true;
+  });
+
+
+// chrome.runtime.onMessage.addListener(
+//   function(request, sender, sendResponse) {
+//     console.log(sender.tab ?
+//                 "from a content script:" + sender.tab.url :
+//                 "from the extension");
+//     if (request.message == "Sending selected toggle option")
+//       sendResponse({received: "Received toggle time option"});
+// 			toggle = request.varToggle;
+// 			console.log("Hump", toggle );
+//   });
