707a8e0804d6bc8ce3403a4522fb6ffe6207d894
Steven Pham <stevenpham408@yahoo.com>
2020-04-27 02:07:50 -0700

    (1) Fully functional with deleting tabs after a certain amount of time
    (2) Cleaned up the code a bit

--- background.js
+++ background.js
@@ -1,86 +1,80 @@
 // Copyright 2018 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
-
-
 'use strict';
 
+var timeUnit = 'min';
+var toggle = false;
+var numTime = 0;
+
+// # of tabs to delete
+var numTabsToDelete = 0;
+
 var numToStop = 0;
-var timeUnit;
-var toggle;
-
-function delayedDelete(tab){
-	console.log("delayedDelete");
-	console.log(timeUnit);
-	setTimeout(function(){
-		chrome.tabs.remove(tab.id)
-		console.log("Time: ", new Date().toString());
-	}, 5000 );
-}
-
-var numToDelete = 0;
+var openTabs = {};
 
 chrome.runtime.onInstalled.addListener(function() {
-	chrome.storage.sync.set({color: '#3aa757'}, function() {
-		console.log("The extension is running.");
-	});
-
-	// chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-	// 	chrome.declarativeContent.onPageChanged.addRules([{
-	// 		conditions: [new chrome.declarativeContent.PageStateMatcher({
-	// 			pageUrl: {hostEquals: 'developer.chrome.com'},
-	// 		})],
-	// 		actions: [new chrome.declarativeContent.ShowPageAction()]
-	// 	}]);
-	// });
+	console.log('The extension is running.')
 });
 
-var openTabs = {};
-chrome.tabs.onCreated.addListener(function(tab) {
-  //numToDelete = numToDelete + 1;
-  console.log("Tab ID: ", tab.id);
-  openTabs[tab.id] = new Date();
-  numToDelete = numToDelete + 1;
-  console.log(openTabs[tab.id].toString());
-  console.log("Length: ", numToDelete);
-	while(toggle == true)
-	{
-		delayedDelete(tab);
-	}
-});
+chrome.runtime.onConnect.addListener(function (externalPort) {
+  externalPort.onDisconnect.addListener(function () {
+    console.log("onDisconnect")
 
-chrome.tabs.onRemoved.addListener(function(tabID, removeInfo) {
-  if (openTabs[tabID]) {
-    console.log("Removing tab with ID: ", tabID);
-    delete openTabs[tabID]
-  }
-  else{
-    console.log("Else: ")
-  }
-});
+		chrome.tabs.onCreated.addListener(function(tab){
+			if(toggle == true){
+				console.log("New tab created. Tab ID: ", tab.id);
+				openTabs[tab.id] = new Date();
+				numTabsToDelete = numTabsToDelete + 1;
+				console.log(openTabs[tab.id].toString());
+				delayedDelete(tab.id);
+			}
 
+			chrome.tabs.onRemoved.addListener(function(tabID, removeInfo){
+				if(tab.id == tabID && toggle == true){
+					console.log("Tab delete detected");
+					delete openTabs[tabID];
+				}
+			});
+		});
+		//
+  });
+  console.log("onConnect")
+});
 
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
-    console.log(sender.tab ?
-                "from a content script:" + sender.tab.url :
-                "from the extension");
-    if (request.message == "Sending selected time and toggle option")
-      sendResponse({received: "Received selected time option"});
-			timeUnit = request.varTime;
-			toggle = request.varToggle;
-			console.log("Sex", timeUnit, toggle);
-			return true;
-  });
+    if (request.message == "User changed toggle value!"){
+			toggle = request.varNewToggle;
+			console.log("New toggle value is: ", toggle)
+		}
+	});
+
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    if (request.message == "User changed time unit!"){
+			timeUnit = request.varNewUnit;
+			console.log("Sexerito");
+		}
+	});
+
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    if (request.message == "User changed field value!"){
+			numTime = request.varNewNumTime;
+			console.log("New field value is: ", numTime)
+		}
+	});
 
 
-// chrome.runtime.onMessage.addListener(
-//   function(request, sender, sendResponse) {
-//     console.log(sender.tab ?
-//                 "from a content script:" + sender.tab.url :
-//                 "from the extension");
-//     if (request.message == "Sending selected toggle option")
-//       sendResponse({received: "Received toggle time option"});
-// 			toggle = request.varToggle;
-// 			console.log("Hump", toggle );
-//   });
+function delayedDelete(tabID){
+	var delayedTime;
+	if(timeUnit == "min") { delayedTime = numTime * 60000 };
+	if(timeUnit == "hr")  { delayedTime = numTime * 3600000 }
+	setTimeout(function(){
+		if(tabID in openTabs){
+			chrome.tabs.remove(tabID)
+			console.log("Time: ", new Date().toString());
+		}
+	}, delayedTime);
+}
