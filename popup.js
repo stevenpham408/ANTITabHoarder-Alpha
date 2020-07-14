4eeb1b89ebbe1494e5822097660615c3e78e3954
Steven Pham <stevenpham408@yahoo.com>
2020-07-13 23:27:40 -0700

    Spacing changes

--- popup.js
+++ popup.js
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 'use strict';
-// var port = chrome.runtime.connect();
+//var port = chrome.runtime.connect();
 
 // event listener for the button inside popup window
 document.addEventListener('DOMContentLoaded', function() {
